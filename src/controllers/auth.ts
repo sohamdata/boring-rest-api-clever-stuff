278e1a448814cb211dd050939f798ece01f8c380
Soham Datta <sohamdatta34@gmail.com>
2023-08-06 20:23:15 +0530

    feat: added login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -29,3 +29,34 @@ export const register = async (req: express.Request, res: express.Response) => {
         return res.sendStatus(400);
     }
 }
+
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.sendStatus(400);
+        }
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if (!user) {
+            return res.status(403).json({ message: 'User not found' });
+        }
+        // console.log(user);
+
+        const { salt, password: dbPass } = user.authentication;
+        const hash = authentication(salt, password);
+
+        if (hash !== dbPass) {
+            return res.status(403).json({ message: 'Invalid password' });
+        }
+
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+        await user.save();
+
+        res.cookie('sessionToken', user.authentication.sessionToken, { domain: 'localhost', path: '/', });
+        return res.status(200).json({ message: 'Logged in', user: user });
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import { register } from '../controllers/auth';
+import { register, login } from '../controllers/auth';
 
 export default (router: express.Router) => {
     router.post('/auth/register', register);
+    router.post('/auth/login', login);
 };
