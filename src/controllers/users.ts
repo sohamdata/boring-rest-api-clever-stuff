c5a3ed30bdb5cb2b0e1431de56df4f1c39ebdc38
Soham Datta <sohamdatta34@gmail.com>
2023-08-07 23:02:19 +0530

    feat: added updateUser ctrller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { getUsers, deleteUserById } from '../db/users';
+import { getUsers, updateUserById, deleteUserById } from '../db/users';
+// import { getUserById } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -16,6 +17,35 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
         const { id } = req.params;
         const deletedUser = await deleteUserById(id);
         return res.json({ message: "User deleted successfully", deleted: deletedUser });
+    }
+    catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const { username, email } = req.body;
+
+        if (!username && !email) {
+            return res.status(400).json({ message: "No fields to update" });
+        }
+
+        // const user = await getUserById(id);
+        // username ? user.username = username : null;
+        // email ? user.email = email : null;
+
+        // await user.save();
+
+        // return res.json({ message: "User updated successfully", updated: user });
+
+        // OR
+
+        const updatedUser = await updateUserById(id, { username, email });
+        return res.json({ message: "User updated successfully", updated: updatedUser });
+
     }
     catch (error) {
         console.log(error);
