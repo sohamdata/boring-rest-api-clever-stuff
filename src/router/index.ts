30c96805a0f35b1ad77673fa0dd64b0a43a8dc99
Soham Datta <sohamdatta34@gmail.com>
2023-08-07 20:14:16 +0530

    feat: added get users controller

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import auth from './auth';
+import users from './users';
 
 const router = express.Router();
 
 export default (): express.Router => {
     auth(router);
+    users(router);
     return router;
 };
